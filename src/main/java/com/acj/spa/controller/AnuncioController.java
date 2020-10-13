655f098abf96f3af44e8aae0c2ceb13b51e06ecb
Alisson <alidias@stj.jus.br>
2020-10-13 20:37:35 -0300

    Ajustando swagger, refatorando classe AnuncioService e ajustando demais coisas.

--- src/main/java/com/acj/spa/controller/AnuncioController.java
+++ src/main/java/com/acj/spa/controller/AnuncioController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,7 +26,7 @@ public class AnuncioController {
     @ApiOperation(value = "Cadastrar anuncio")
     @PreAuthorize("isAuthenticated()")
     @PostMapping
-    public ResponseEntity<String> cadastrar(@RequestBody(required = false) final AnuncioDTO anuncioDTO) {
+    public ResponseEntity<String> cadastrar(@RequestBody(required = true) final AnuncioDTO anuncioDTO) {
         return ResponseEntity.ok(anuncioService.cadastrar(anuncioDTO));
     }
 
@@ -49,8 +48,8 @@ public class AnuncioController {
     @ApiOperation(value = "Obter todos anuncio do usuário logado.")
     @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/meus-anuncios")
-    public ResponseEntity<List<AnuncioRegistrosDTO>> obterMeusAnuncios(Authentication authenticatioToken) {
-        return new ResponseEntity<>(anuncioService.listarMeusAnuncios(), HttpStatus.OK);
+    public ResponseEntity<List<AnuncioRegistrosDTO>> obterMeusAnuncios() {
+        return ResponseEntity.ok(anuncioService.listarMeusAnuncios());
     }
 
     @ApiOperation(value = "Obter todos anuncios com filtro.")
