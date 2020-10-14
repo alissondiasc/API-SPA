c8e6a391f76d5ce2749f96e31e7c5326790a5afe
Alisson <alidias@stj.jus.br>
2020-10-14 12:01:16 -0300

    Implementando configuração de autorização para docuemntação com swagger

--- src/main/java/com/acj/spa/config/swagger/SwaggerConfig.java
+++ src/main/java/com/acj/spa/config/swagger/SwaggerConfig.java
@@ -8,37 +8,53 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig  extends WebMvcConfigurationSupport {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-@Bean
-public Docket apiDocket() {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
-    return new Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(metaData())
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("com.acj.spa.controller"))
-            .paths(PathSelectors.any())
-            .build();
-}
+    @Bean
+    public Docket actuatorApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.acj.spa.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData())
+                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()));
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/.*")).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return Collections.singletonList(new SecurityReference("Bearer", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
                 .title("SPA - API")
                 .description("\"Sistema de prestação e contratação de serviços\"")
-                .version("1.0.0")
+                .license("openSource")
                 .build();
     }
 
@@ -50,4 +66,5 @@ public Docket apiDocket() {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
 }
